c1f3adae157b2538fea64e4e7d9d5f095375333b
KhanA6N2 <abdun.nafi.khan@gmail.com>
2025-06-01 16:38:29 -0500

    Changed collision layers (check over again later)

--- Scripts/cursor_indicator.gd
+++ Scripts/cursor_indicator.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	global_position = get_global_mouse_position()
 	
 #func _unhandled_input(event: InputEvent) -> void:
